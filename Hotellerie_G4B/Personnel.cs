e83f497154a2ddb92d4f74019ef73e5556cfdc3f
Mattéo Da Cunha <pl07ccc@eduvaud.ch>
2023-12-15 11:12:08 +0100

    Fix environnement and code

---
+++ Hotellerie_G4B/Personnel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4bDev_Hôtellerie
+{
+    public class Personnel
+    {
+
+        public int Id { get; set; }
+        private string firstName;
+        private string lastName;
+        private string poste;
+
+        public Personnel(string firstName, string lastName, string poste)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.poste = poste;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Poste
+        {
+            get
+            {
+                return poste;
+            }
+            set
+            {
+                poste = value;
+            }
+        }
+
+        public void AjoutPersonnel()
+        {
+            DBconnector.AjoutPersonnel(firstName, lastName, poste);
+        }
+
+        public void ModifInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SuppPersonnel(int id)
+        {
+            DBconnector.SuppPersonnel(id);
+        }
+    }
+}
